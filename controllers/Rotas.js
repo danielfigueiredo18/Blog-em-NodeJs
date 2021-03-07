0b621f23b4c249d93f8a2a491cc0f3c45ac0f01d
Daniboy <danielfigueiredo18@outlook.com>
2021-03-07 18:37:29 -0300

    vercel

--- controllers/Rotas.js
+++ controllers/Rotas.js
@@ -6,16 +6,4 @@ router.get("/assets/js/style.js",(req,res)=>
     res.sendFile(path.resolve("./assets/js/style.js"))
 )
 
-router.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`
-    res.setHeader('Content-Type', 'text/html')
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
-  })
-  
-  router.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params
-    res.end(`Item: ${slug}`)
-  })
-
 module.exports = router;
