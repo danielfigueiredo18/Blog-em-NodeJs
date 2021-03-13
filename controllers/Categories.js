536c2564f399763194e71e6cb70f83e988462691
Daniboy <danielfigueiredo18@outlook.com>
2021-03-13 09:14:43 -0300

    update categories

--- controllers/Categories.js
+++ controllers/Categories.js
@@ -7,6 +7,28 @@ router.get("/admin/categories/new",(req,res)=>{
     res.render("admin/categories/new")
 })
 
+router.get("/admin/categories",(req,res)=>{
+    Category.findAll().then(categories => {
+       res.render("admin/categories/index",{categories: categories}) 
+    })   
+})
+
+router.get("/admin/categories/edit/:id",(req,res)=>{
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/categories")
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render("admin/categories/edit",{category: category}) 
+        }else{
+            res.redirect("/admin/categories")
+        }
+    }).catch(error => {
+        res.redirect("/admin/categories")
+    })   
+})
+
 router.post("/categories/save",(req,res)=>{
     var title = req.body.title
     if(title != undefined){
@@ -17,12 +39,6 @@ router.post("/categories/save",(req,res)=>{
     }else{
         res.redirect("/admin/categories/new")
     }
-})
-
-router.get("/admin/categories",(req,res)=>{
-    Category.findAll().then(categories => {
-       res.render("admin/categories/index",{categories: categories}) 
-    })   
 })
 
 router.post("/categories/delete", (req,res) =>{
@@ -39,6 +55,22 @@ router.post("/categories/delete", (req,res) =>{
         }else{
             res.redirect("/admin/categories")
         }
+    }else{
+        res.redirect("/admin/categories")
+    }
+})
+
+router.post("/categories/update", (req,res) =>{
+    var id = req.body.id
+    var title = req.body.title
+    if(id != undefined && title != undefined){
+            Category.update(
+                {title: title, slug: slugifly(title)},{
+                    where: {id: id}
+                }
+            ).then(()=>{
+                res.redirect("/admin/categories")
+            })
     }else{
         res.redirect("/admin/categories")
     }
