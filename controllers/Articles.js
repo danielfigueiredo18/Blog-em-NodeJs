74b2974d0b61ddd1088ca90f4d58828cb1d178e2
Daniboy <danielfigueiredo18@outlook.com>
2021-04-17 11:32:18 -0300

    Paginação

--- controllers/Articles.js
+++ controllers/Articles.js
@@ -47,7 +47,10 @@ router.get("/articles/page/:num",(req,res)=>{
   
     Article.findAndCountAll({
         limit: 4,
-        offset: offset
+        offset: offset,
+        order: [
+            ['id','desc']
+        ]
     }).then(articles=>{
         var next;
         if(offset + 4 >= articles.count){
@@ -56,6 +59,7 @@ router.get("/articles/page/:num",(req,res)=>{
             next = true
         }
         var result = {
+            page: parseInt(page),
             next: next,
             articles: articles
         }
